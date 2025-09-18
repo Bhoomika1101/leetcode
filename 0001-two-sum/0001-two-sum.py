f837c2a9fa573a2d63362f9ad2ad3fbc65985df7
Bhoomika1101 <bhoomidheeravath@gmail.com>
2025-09-18 22:30:08 +0530

    Time: 3 ms (58.58%), Space: 13.1 MB (76.96%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, nums, target):
+        num_map = {}  # stores number as key and its index as value
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in num_map:
+                return [num_map[complement], i]
+            num_map[num] = i
